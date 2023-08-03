79f967ecbf96d69fed184cde242077ab1b7e5f04
qwer1227 <seongmin123456>
2023-08-03 15:07:09 +0900

    컴포넌트 스캔

--- src/main/java/hello/core/CoreApplication.java
+++ src/main/java/hello/core/CoreApplication.java
@@ -2,11 +2,14 @@ package hello.core;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @SpringBootApplication
+@ServletComponentScan
 public class CoreApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(CoreApplication.class, args);
 	}
 
